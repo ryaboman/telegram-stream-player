d49f2b6de84c1c98b3a33dbfb0745c07c3fc4ee5
aleksey <ryabov@radiomind.ru>
2024-11-20 22:24:10 +0300

    Функция воспроизведения звука вынесена в отдельный модуль

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-#!/usr/bin/python3
-import os
-import signal
-import subprocess
-import sys
-import time
-import shutil
-
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import Default, DefaultBotProperties
 from aiogram.types import Message
@@ -16,44 +8,8 @@ from core.settings import settings
 from aiogram.filters import Command
 from aiogram import F
 from core.utils.commands import set_commands
+from core.handlers.stream import stream_run
 from core.utils.statesform import StepsViewProjects
-
-    
-def remove_contents() :
-    folder = 'test_ffmpeg/'
-    for filename in os.listdir(folder):
-        file_path = os.path.join(folder, filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-        except Exception as e:
-            print('Failed to delete %s. Reason: %s' % (file_path, e))
-
-def streamer(url_to_stream: str) :
-        
-    _ffmpeg_process_inner = subprocess.Popen(('ffmpeg', '-re', '-i', '22.mp3', '-hls_list_size', '30',
-                                              '-hls_flags', 'delete_segments+append_list+omit_endlist',
-                                              '-f', 'hls', 'test_ffmpeg/out.m3u8'))
-        
-    _ffmpeg_process_outter = subprocess.Popen(('ffmpeg', '-stream_loop', '-1', '-re', '-f', 'hls', '-i',
-                                               'test_ffmpeg/out.m3u8', '-c:v', 'libx264', '-preset',
-                                               'veryfast', '-b:v', '3500k', '-maxrate', '3500k', '-bufsize',
-                                               '7000k', '-pix_fmt', 'yuv420p', '-g', '50', '-c:a', 'aac', '-b:a',
-                                               '160k', '-ac', '2', '-ar', '44100', '-f', 'flv', url_to_stream))
-
-    print('Proceses is started')
-
-    os.waitpid(_ffmpeg_process_inner.pid, 0)
-
-    print('Переключаем песню')
-    
-    #remove_contents()
-    
-    _ffmpeg_process_inner = subprocess.Popen(('ffmpeg', '-re', '-i', '23.mp3', '-hls_list_size', '30', '-hls_flags', 'delete_segments+append_list+omit_endlist', '-f', 'hls', 'test_ffmpeg/out.m3u8'))#, stdout=subprocess.PIPE
-
-    return
 
 
 async def start_bot(bot: Bot):
@@ -75,7 +31,7 @@ async def start():
     dp.startup.register(start_bot)
     dp.shutdown.register(stop_bot)
 
-    streamer(settings.bots.url_to_rtmps)
+    dp.callback_query.register(stream_run, F.data == 'play_track')
 
     dp.message.register(get_start, Command(commands=['start', 'run']))
 
